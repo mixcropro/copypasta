07a491b378d9ec43e8f92ad8b525eba4d8315799
mixcropro <62208129+mixcropro@users.noreply.github.com>
2020-04-17 14:31:09 +0200

    Add files via upload

--- passta.py
+++ passta.py
@@ -1,32 +1,27 @@
-import pyperclip
 from datetime import datetime
 from tkinter import Tk
-
+import os.path
+from time import *
 
 pasta = Tk().clipboard_get()
 # datetime object containing current date and time
 now = datetime.now()
 # dd/mm/YY H:M:S
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+dt_string = now.strftime("%d/%m/%Y")
+postoji = os.path.isfile('MyFile.txt')
+if postoji:
+    f = open("MyFile.txt")
+    last_line = f.readlines()[-1]
+    f.close()
 
-file1 = open("MyFile.txt", "a")
-file1.write("\n" + pasta + " - text was copied at " + dt_string)
-file1.close()
-
-'''while True:
-    file1 = open("MyFile.txt", "w+")
-    lineList = file1.readlines()
-    kraj = lineList[-1]
-    file1.close()
-    if pasta != kraj:
-        file1.write("\n" + pasta + " - text was copied at " + dt_string)
+    if last_line != pasta + " - text was copied on " + dt_string:
+        file1 = open("MyFile.txt", "a")
+        file1.write("\n" + pasta + " - text was copied on " + dt_string)
+        file1.close()
     else:
-        continue'''
-
-
-
-
-
-
-
-
+        print("The text you copied is the same as the last saved link!")
+        sleep(4)
+else:
+    file1 = open("MyFile.txt", "a")
+    file1.write("All of your saved links")
+    file1.close()
